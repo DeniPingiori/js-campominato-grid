bb3eada8a8fe6aa0156a6b4262dc4833ea7b3aa9
Denise Pingiori <pingiorid@gmail.com>
2024-07-09 17:15:09 +0200

    [upd] upd function square

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
+//funzione crea casella della griglia
+function createSingleSquare(num){
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.innerText = num;
+}
+
 //richiamo il pulsante 
 const button = document.getElementById('play');
 
 //evento click
 button.addEventListener('click', function(){
-    console.log('cominciamo la partita');
-})
+    //console.log('cominciamo la partita');
+
+    //recupero elem che dovrà contenere la griglia dal dom
+    const grid = document.getElementById('grid');
+});
+
