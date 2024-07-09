6135499752f61c37a24b239080fde139c38c74ec
Denise Pingiori <pingiorid@gmail.com>
2024-07-09 17:07:51 +0200

    [upd] upd call back button and event click

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//richiamo il pulsante 
+const button = document.getElementById('play');
+
+//evento click
+button.addEventListener('click', function(){
+    console.log('cominciamo la partita');
+})
